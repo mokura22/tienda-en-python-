697a0067f96ee8a4a68f93750dab1e6339883446
karina <karina@eloyaltymarketing.com>
2022-10-25 12:04:33 -0300

    actualizacion 25/10

--- inventory/views.py
+++ inventory/views.py
@@ -5,3 +5,16 @@ from .models import Product
 def list_products(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
+def create_product(request):
+    if request.method =='POST' :
+        name = request.POST['name']
+        price = request.POST['price']
+        stock = request.POST['stock']
+        product = Product(price=price, name=name, stock=stock)
+        product.save()
+        return redirect('/')
+    else:
+        return redirect('/')
+
+def delete_product(request):
+    pass        
